565629286f5ba1d263ce373d9a1e265465e47de2
luckyfishisdashen <2383690354@qq.com>
2022-11-25 12:27:04 +0800

    更新 readme.md

--- LuckyFish.MarkdownSharp.Lib/Markdown.cs
+++ LuckyFish.MarkdownSharp.Lib/Markdown.cs
@@ -1,54 +1,56 @@
 ﻿using System.Text;
 
 namespace LuckyFish.MarkdownSharp.Lib
-
-public class Markdown
 {
-    private readonly List<MarkdownNode> RootNode;
-    private readonly StringBuilder Builder;
-    public string context;
-    /// <summary>
-    /// 
-    /// </summary>
-    /// <param name="Context"></param>
-    public Markdown(string Context)
+    public class Markdown
     {
-        this.context = Context;
-        RootNode = new List<MarkdownNode>();
-        Builder = new StringBuilder();
-        Transform();
-        ToStringBuilder();
-    }
-    /// <summary>
-    /// 翻译，将context翻译成List<MarkdownNode>(RootNode)
-    /// </summary>
-    private void Transform()
-    {
-        string line = "";
-        bool isCodeBlock = false;
-        using StreamReader reader =
-            new StreamReader(new MemoryStream(Encoding.Unicode.GetBytes(context)));
-        while ((line = reader.ReadLine()) is not null)
+        private readonly List<MarkdownNode> RootNode;
+        private readonly StringBuilder Builder;
+        public string context;
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="Context"></param>
+        public Markdown(string Context)
         {
-            line = line.TrimStart(' ');
-            if (!isCodeBlock && string.IsNullOrEmpty(line))
+            this.context = Context;
+            RootNode = new List<MarkdownNode>();
+            Builder = new StringBuilder();
+            Transform();
+            ToStringBuilder();
+        }
+        /// <summary>
+        /// 翻译，将context翻译成List<MarkdownNode>(RootNode)
+        /// </summary>
+        private void Transform()
+        {
+            string line = "";
+            bool isCodeBlock = false;
+            using StreamReader reader =
+                new StreamReader(new MemoryStream(Encoding.Unicode.GetBytes(context)));
+            while ((line = reader.ReadLine()) is not null)
             {
-                continue;
-            }
+                line = line.TrimStart(' ');
+                if (!isCodeBlock && string.IsNullOrEmpty(line))
+                {
+                    continue;
+                }
 
-            if (1 == 1)
-            {
+                if (1 == 1)
+                {
 
+                }
             }
         }
-    }
-    /// <summary>
-    /// 将RootNode翻译成StringBuild
-    /// </summary>
-    private void ToStringBuilder()
-    {
+        /// <summary>
+        /// 将RootNode翻译成StringBuild
+        /// </summary>
+        private void ToStringBuilder()
+        {
 
+        }
+        public string ToHTML() => Builder.ToString();
+        public List<MarkdownNode> GetList() => RootNode;
     }
-    public string ToHTML() => Builder.ToString();
-    public List<MarkdownNode> GetList() => RootNode;
 }
+
--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 # LuckyFish.MarkdownSharp
+  
+之前看到武清云写了一个，于是我也打算写一个
